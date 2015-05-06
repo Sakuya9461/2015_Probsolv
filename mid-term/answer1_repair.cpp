491bfc59df31f580e1eeccc9fe3844980063af84
Sakuya <sakuya9461@gmail.com>
2015-05-06 15:57:34 +0900

    repairing..

---
+++ mid-term/answer1_repair.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LINE 1000
+int main()
+{
+	int cnt = 0;
+	for (int a = 1; a < LINE; a++)
+	{
+		for (int b = a; b < LINE; b++)
+		{
+			int c = b*b + a*a;
+			if (c <= 1000*1000)
+				cnt++;
+			else
+				break;
+		}
+	}
+	printf("%d", cnt);
+}
